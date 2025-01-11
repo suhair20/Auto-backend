32eeb3df7771377861f1a57c2b0cd75204b593ff
suhair20 <moidheensuhair@gmail.com>
2025-01-11 13:22:27 +0530

    css

--- server.js
+++ server.js
@@ -16,8 +16,13 @@ connectDB()
 const app=express()
 const port =5000
 
+// app.use(cors({
+//     origin: 'http://localhost:5173', 
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+// }));
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://auto-frontend-8idj-moidheen-suhairs-projects.vercel.app/', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
