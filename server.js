c38088fb650a5afb953294ed7d7982c7a790ccaf
suhair20 <moidheensuhair@gmail.com>
2025-01-16 11:39:06 +0530

    commit

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/', (req, res) => {
+    res.send('Backend is running!');
+  });
+
 app.use('/user',userRoute)
 app.use('/admin',adminRoute)
 app.use('/driver',driverRoute)
